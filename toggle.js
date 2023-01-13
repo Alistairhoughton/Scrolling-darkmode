5c2c97c66283c37e4d83f289d97cceeed6052c9a
Alistair Houghton <alistair.g.houghton@gmail.com>
2023-01-13 23:40:31 +0000

    updated

--- toggle.js
+++ toggle.js
@@ -2,31 +2,22 @@ const toggle = document.getElementById("toggle");
 const sections = document.getElementsByClassName("section");
 
 toggleHandler = () => {
-   
-
-if (toggle.checked == false) {
-   console.log("true");
-   sections[0].setAttribute("data-bg", "#23b296");
-   sections[1].setAttribute("data-bg", "#f15f61");
-   sections[2].setAttribute("data-bg", "#DDBCF7");
-   sections[3].setAttribute("data-bg", "#fca971");
-   sections[0].classList.remove("dark");
-
-   
-} else {
-   console.log("false");
-   sections[0].setAttribute("data-bg", "grey");
-   sections[1].setAttribute("data-bg", "grey");
-   sections[2].setAttribute("data-bg", "grey");
-   sections[3].setAttribute("data-bg", "grey");
-
-   sections[0].classList.add("dark")
-}
-
+  if (toggle.checked == false) {
+    console.log("true");
+    sections[0].setAttribute("data-bg", "#23b296");
+    sections[1].setAttribute("data-bg", "#f15f61");
+    sections[2].setAttribute("data-bg", "#DDBCF7");
+    sections[3].setAttribute("data-bg", "#fca971");
+  } else {
+    console.log("false");
+    sections[0].setAttribute("data-bg", "grey");
+    sections[1].setAttribute("data-bg", "grey");
+    sections[2].setAttribute("data-bg", "grey");
+    sections[3].setAttribute("data-bg", "grey");
+  }
 
   console.log([...sections]);
   console.log(sections[0].dataset.bg);
-  
 };
 
 toggle.addEventListener("click", toggleHandler);
